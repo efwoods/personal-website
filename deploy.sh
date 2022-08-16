2242f438d4ca3d2545a24c32d315dc7c615f609c
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-08-16 09:28:44 -0400

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm run build        
-git add dist && git commit -m 'adding dist subtree'
+git add dist && git commit -m 'updating dist subtree' --allow-empty
 git subtree push --prefix dist origin gh-pages     
